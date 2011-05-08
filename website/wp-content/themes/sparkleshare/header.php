d70e5c8fd030a081b4959fb0dc110d56107ca2f4
Hylke Bons <hylkebons@gmail.com>
2011-05-08 10:45:43 +0100

    index: don't duplicate title

--- website/wp-content/themes/sparkleshare/header.php
+++ website/wp-content/themes/sparkleshare/header.php
@@ -3,7 +3,7 @@
   <head
     <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
     <meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
-    <title><?php bloginfo('name'); ?> - <?php bloginfo('tagline'); ?><?php wp_title(' &raquo; '); ?></title>
+    <title>SparkleShare - Sharing work made easy <?php wp_title(' &raquo; '); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/style/default.css" type="text/css" media="screen" />
     <link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
     <link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
