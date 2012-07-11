79aed000cf179b76ced51ecdb641874317519c5c
Hylke Bons <hylkebons@gmail.com>
2012-07-11 19:50:56 +0200

    footer: Add link to 6sync

--- website/wp-content/themes/sparkleshare/footer.php
+++ website/wp-content/themes/sparkleshare/footer.php
@@ -1,4 +1,9 @@
-    <div id="copyright">&copy; Copyright 2011<br/>The SparkleShare Community</div>
-    
+    <div id="copyright">
+      &copy; Copyright 2011<br/>The SparkleShare Community<br>
+      <br>
+      Hosting kindly provided by:<br>
+      <br>
+      <a href="http://www.6sync.com/"><img src="<?php bloginfo('stylesheet_directory'); ?>/img/6sync.png" alt="6sync"></a>
+    </div>
   </body>
 </html>
